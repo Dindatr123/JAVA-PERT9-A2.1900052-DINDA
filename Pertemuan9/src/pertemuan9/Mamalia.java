6b87faff00cdd2aaa1f89e698773d484ca0dd5b0
Dindatr123 <dindatrirahmasari@gmail.com>
2019-11-14 11:05:48 +0700

    Pertemuan 9

---
+++ Pertemuan9/src/pertemuan9/Mamalia.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan9;
+
+/**
+ *
+ * @author 
+ * Nama : Dinda Tri Rahmasari
+ * Kelas : TI-1A
+ * NIM : A2.1900052
+ */
+public class Mamalia {
+    public static int jumlahKambing;
+}
